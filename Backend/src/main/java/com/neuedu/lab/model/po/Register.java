7d7a269ba0461be985f7a3643d5cb830deb92afb
David <976533706@qq.com>
2019-05-28 16:33:59 +0800

    register实体类

--- Backend/src/main/java/com/neuedu/lab/model/po/Register.java
+++ Backend/src/main/java/com/neuedu/lab/model/po/Register.java
@@ -1,40 +1,67 @@
 package com.neuedu.lab.model.po;
 
 public class Register {
-    private Integer registerInfoId;
-    private Integer registerInfoState;
-    private Double registerInfoFee;
-    private String registerInfoPayType;
+    private Integer register_info_id;
+    private Integer register_info_state;
+    private Double register_info_fee;
+    private String register_info_pay_type;
+    private Integer register_info_doctor_id;
+    private Integer register_info_patient_id;
+    private Integer register_info_user_id;
 
-    public Integer getRegisterInfoId() {
-        return registerInfoId;
+    public Integer getRegister_info_id() {
+        return register_info_id;
     }
 
-    public void setRegisterInfoId(Integer registerInfoId) {
-        this.registerInfoId = registerInfoId;
+    public void setRegister_info_id(Integer register_info_id) {
+        this.register_info_id = register_info_id;
     }
 
-    public Integer getRegisterInfoState() {
-        return registerInfoState;
+    public Integer getRegister_info_state() {
+        return register_info_state;
     }
 
-    public void setRegisterInfoState(Integer registerInfoState) {
-        this.registerInfoState = registerInfoState;
+    public void setRegister_info_state(Integer register_info_state) {
+        this.register_info_state = register_info_state;
     }
 
-    public Double getRegisterInfoFee() {
-        return registerInfoFee;
+    public Double getRegister_info_fee() {
+        return register_info_fee;
     }
 
-    public void setRegisterInfoFee(Double registerInfoFee) {
-        this.registerInfoFee = registerInfoFee;
+    public void setRegister_info_fee(Double register_info_fee) {
+        this.register_info_fee = register_info_fee;
     }
 
-    public String getRegisterInfoPayType() {
-        return registerInfoPayType;
+    public String getRegister_info_pay_type() {
+        return register_info_pay_type;
     }
 
-    public void setRegisterInfoPayType(String registerInfoPayType) {
-        this.registerInfoPayType = registerInfoPayType;
+    public void setRegister_info_pay_type(String register_info_pay_type) {
+        this.register_info_pay_type = register_info_pay_type;
+    }
+
+    public Integer getRegister_info_doctor_id() {
+        return register_info_doctor_id;
+    }
+
+    public void setRegister_info_doctor_id(Integer register_info_doctor_id) {
+        this.register_info_doctor_id = register_info_doctor_id;
+    }
+
+    public Integer getRegister_info_patient_id() {
+        return register_info_patient_id;
+    }
+
+    public void setRegister_info_patient_id(Integer register_info_patient_id) {
+        this.register_info_patient_id = register_info_patient_id;
+    }
+
+    public Integer getRegister_info_user_id() {
+        return register_info_user_id;
+    }
+
+    public void setRegister_info_user_id(Integer register_info_user_id) {
+        this.register_info_user_id = register_info_user_id;
     }
 }
