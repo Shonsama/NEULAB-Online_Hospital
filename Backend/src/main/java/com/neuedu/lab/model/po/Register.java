3b3b89c058adf6632c5813d034d5992bfcad1c95
David <976533706@qq.com>
2019-05-28 14:33:59 +0800

    数据库DDL

--- Backend/src/main/java/com/neuedu/lab/model/po/Register.java
+++ Backend/src/main/java/com/neuedu/lab/model/po/Register.java
@@ -1,5 +1,14 @@
 package com.neuedu.lab.model.po;
 
 public class Register {
+    private Integer registerInfoId;
+    private Integer registerInfoState;
+    private Double registerInfoFee;
+    private String registerInfoPayType;
+
+
+
+
+
 
 }
