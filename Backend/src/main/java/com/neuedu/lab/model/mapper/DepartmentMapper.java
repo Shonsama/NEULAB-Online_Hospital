56573f4755ac0bc07e5d41e6aedc770d98da9e25
DanielLee343 <30738558+DanielLee343@users.noreply.github.com>
2019-05-28 16:22:15 +0800

    create department

--- Backend/src/main/java/com/neuedu/lab/model/mapper/DepartmentMapper.java
+++ Backend/src/main/java/com/neuedu/lab/model/mapper/DepartmentMapper.java
@@ -1,5 +1,12 @@
 package com.neuedu.lab.model.mapper;
 
-public interface DepartmentMapper {
+import com.neuedu.lab.model.po.Department;
 
+import java.util.List;
+
+public interface DepartmentMapper {
+    public List<Department> getDepartment(String department_id);
+    public void addDepartment(Department department);
+    public void deleteDepartment(Department department);
+    public void updateDepartment(Department department);
 }
