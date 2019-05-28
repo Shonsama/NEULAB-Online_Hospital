09c934111be354c491e8ba882ca31e7d721eb8ec
DanielLee343 <30738558+DanielLee343@users.noreply.github.com>
2019-05-28 13:37:59 +0800

    update db

--- Backend/src/main/java/com/neuedu/lab/model/po/Constant.java
+++ Backend/src/main/java/com/neuedu/lab/model/po/Constant.java
@@ -5,5 +5,30 @@ import java.util.List;
 public class Constant {
     private int constant_id;
     private String constant_name;
+
+    public int getConstant_id() {
+        return constant_id;
+    }
+
+    public void setConstant_id(int constant_id) {
+        this.constant_id = constant_id;
+    }
+
+    public String getConstant_name() {
+        return constant_name;
+    }
+
+    public void setConstant_name(String constant_name) {
+        this.constant_name = constant_name;
+    }
+
+    public String getConstant_type() {
+        return constant_type;
+    }
+
+    public void setConstant_type(String constant_type) {
+        this.constant_type = constant_type;
+    }
+
     private String constant_type;
 }
