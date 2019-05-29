5e9c7291caecd0f458c378d533d2259e518710e3
DanielLee343 <30738558+DanielLee343@users.noreply.github.com>
2019-05-29 09:30:29 +0800

    fix previous bugs

--- Backend/src/main/java/com/neuedu/lab/model/mapper/DepartmentMapper.java
+++ Backend/src/main/java/com/neuedu/lab/model/mapper/DepartmentMapper.java
@@ -5,7 +5,7 @@ import com.neuedu.lab.model.po.Department;
 import java.util.List;
 
 public interface DepartmentMapper {
-    public List<Department> getAllDepartments();
+    public List<Department> getDepartment(String department_id);
     public void addDepartment(Department department);
     public void deleteDepartment(Department department);
     public void updateDepartment(Department department);
