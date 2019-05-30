8202ca718f2f9233773caa72563a9d96b530af58
DanielLee343 <30738558+DanielLee343@users.noreply.github.com>
2019-05-30 10:56:10 +0800

    commit department demo and sql

---
+++ Backend/src/main/java/com/neuedu/lab/controller/NonMedicineController.java
@@ -0,0 +1,60 @@
+package com.neuedu.lab.controller;
+
+
+import com.neuedu.lab.model.po.NonMedicine;
+import com.neuedu.lab.model.service.NonMedicineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/nonmedicine")
+public class NonMedicineController {
+    @Autowired
+    private NonMedicineService nonMedicineService;
+
+    @RequestMapping("/getall")
+//    返回所有非药品信息
+    public List<NonMedicine> getAllNonMedicines(){
+        return nonMedicineService.getAllNonMedicines();
+    }
+
+    @RequestMapping("/get")
+//    查询非药品信息
+    public List<NonMedicine> getNonMedicine(@RequestParam String non_medicine_id){
+        return nonMedicineService.getNonMedicine(non_medicine_id);
+    }
+
+    @RequestMapping("/add")
+    public String addNonMedicine(@RequestParam NonMedicine nonMedicine){
+        if(nonMedicineService.addNonMedicine(nonMedicine)){
+            return "{\"result\":\"success\"}";
+        }
+        else {
+            return "{\"result\":\"fail\"}";
+        }
+    }
+
+    @RequestMapping("/update")
+    public String updateNonMedicine(@RequestParam NonMedicine nonMedicine){
+        if(nonMedicineService.updateNonMedicine(nonMedicine)){
+            return "{\"result\":\"success\"}";
+        }
+        else {
+            return "{\"result\":\"fail\"}";
+        }
+    }
+
+    @RequestMapping("/delete")
+    public String deleteNonMedicine(@RequestParam NonMedicine nonMedicine){
+        if(nonMedicineService.deleteNonMedicine(nonMedicine)){
+            return "{\"result\":\"success\"}";
+        }
+        else {
+            return "{\"result\":\"fail\"}";
+        }
+    }
+}
