fc86471b8c6b2db86a06f3330e53b0fb1127f139
DanielLee343 <30738558+DanielLee343@users.noreply.github.com>
2019-06-10 18:46:11 +0800

    commit department demo and sql

---
+++ Backend/src/main/java/com/neuedu/lab/model/mapper/DoctorWorkloadMapper.java
@@ -0,0 +1,27 @@
+package com.neuedu.lab.model.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+public interface DoctorWorkloadMapper {
+    Integer getVisitorsNum(@Param("start_time") Date start_time, @Param("end_time")Date end_time, @Param("doctor_id")Integer doctor_id);
+
+    Integer getBillsNum(@Param("start_time")Date start_time,@Param("end_time")Date end_time, @Param("doctor_id")Integer doctor_id);
+
+    BigDecimal getRegisterFee(@Param("start_time")Date start_time,@Param("end_time")Date end_time, @Param("doctor_id")Integer doctor_id);
+
+    BigDecimal getMedicalSkillInspect(@Param("start_time")Date start_time,@Param("end_time")Date end_time, @Param("doctor_id")Integer doctor_id);
+
+    BigDecimal getMedicalSkillCheck(@Param("start_time")Date start_time,@Param("end_time")Date end_time, @Param("doctor_id")Integer doctor_id);
+
+    BigDecimal getMedicalSkillDispose(@Param("start_time")Date start_time,@Param("end_time")Date end_time, @Param("doctor_id")Integer doctor_id);
+
+    BigDecimal getPrescriptionWest(@Param("start_time")Date start_time,@Param("end_time")Date end_time, @Param("doctor_id")Integer doctor_id);
+
+    BigDecimal getPrescriptionChinese(@Param("start_time")Date start_time,@Param("end_time")Date end_time, @Param("doctor_id")Integer doctor_id);
+
+    List<Integer> getDoctorsId();
+}
