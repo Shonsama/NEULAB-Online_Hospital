3c1807fc42bef0c63a71c9d80da580dac4125386
David <976533706@qq.com>
2019-06-10 18:06:49 +0800

    医生接诊 Bug

--- Backend/src/main/java/com/neuedu/lab/model/mapper/DoctorMapper.java
+++ Backend/src/main/java/com/neuedu/lab/model/mapper/DoctorMapper.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface DoctorMapper {
     public List<Doctor> getAllDoctorsByDepartment(@Param("department_id") String department_id, @Param("doctor_register_level_id") Integer doctor_register_level_id);
 
-    public void treat(Integer register_id,String state);
+    public void treat(@Param("register_id") Integer register_id,@Param("state") String state);
 }
