9b77fcd44ceed37e8985be93d4f61f233d574a37
DanielLee343 <30738558+DanielLee343@users.noreply.github.com>
2019-06-12 15:35:53 +0800

    commit department demo and sql

--- Backend/src/main/java/com/neuedu/lab/model/service/PatientService.java
+++ Backend/src/main/java/com/neuedu/lab/model/service/PatientService.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.neuedu.lab.Utils.ConstantUtils;
 import com.neuedu.lab.model.mapper.PatientMapper;
 import com.neuedu.lab.model.po.Patient;
+import com.neuedu.lab.model.po.PatientUser;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -15,30 +16,117 @@ public class PatientService {
     @Resource
     private PatientMapper patientMapper;
 
-    public List<Patient> getAllPatients(){
+    public List<Patient> getAllPatients() {
         return patientMapper.getAllPatients();
     }
 
-    public JSONObject addPatient(Patient patient){
+    public JSONObject addPatient(Patient patient) {
         try {
             patientMapper.addPatient(patient);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return ConstantUtils.responseFail();
         }
         return ConstantUtils.responseSuccess(patient);
     }
 
-    public Patient getPatientByRecordId(Integer patient_id){
+    public Patient getPatientByRecordId(Integer patient_id) {
         return patientMapper.getPatientByRecordId(patient_id);
     }
 
-    public List<Patient> getPatientByDoctorId(String doctor_id){
+    public List<Patient> getPatientByDoctorId(String doctor_id) {
         return patientMapper.getPatientByDoctorId(doctor_id);
     }
 
-    public List<Patient> getPatientByDepartmentId(String department_id){
+    public List<Patient> getPatientByDepartmentId(String department_id) {
         return patientMapper.getPatientByDepartmentId(department_id);
+    }
+
+    public boolean checkIfExistAccount(String patient_account) {
+        Integer size = patientMapper.getAllAccounts().size();
+        Integer count = 0;
+        for (int i = 0; i < size; i++) {
+            if (!patientMapper.getAllAccounts().get(i).equals(patient_account)) {
+                count++;
+            }
+        }
+        if (count == size) {
+            return false;//不存在
+        } else
+            return true;
+    }
+
+    public boolean checkIfExistId(Integer patient_record_id) {
+        Integer size = patientMapper.getAllPatientRecordIds().size();
+        for (int i = 0; i < size; i++) {
+            if (patientMapper.getAllPatientRecordIds().get(i).equals(patient_record_id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    //登录
+/*    public JSONObject checkUserValid() {
+
+    }*/
+
+    //    注册用户
+    public JSONObject signUp(String patient_account, String patient_password) {
+        if (!checkIfExistAccount(patient_account)) {
+            //允许注册
+            PatientUser patientUser = new PatientUser();
+            patientUser.setPatient_account(patient_account);
+            patientUser.setPatient_password(patient_password);
+            patientMapper.addPatientUser(patientUser);
+            return ConstantUtils.responseSuccess("注册成功");
+        } else {
+            return ConstantUtils.responseFail("注册失败，已有相同account存在！");
+        }
+    }
+
+    //绑定已有病历号
+    public JSONObject boundExistedRecord(String patient_account, Integer patient_record_id) {
+        if (checkIfExistId(patient_record_id)) {//有该病历号
+            patientMapper.updatePatientUserRecordId(patient_record_id,patient_account);
+            return ConstantUtils.responseSuccess("绑定成功");
+        }else//无该病历号，无法绑定
+            return ConstantUtils.responseFail("绑定失败");
+    }
+
+    //新建病历
+    public JSONObject createNewRecord(Patient patient,String patient_account) {
+        addPatient(patient);
+        patientMapper.updatePatientUserRecordId(patient.getPatient_record_id(),patient_account);
+        return ConstantUtils.responseSuccess("新建用户成功");
+    }
+
+    public PatientUser getPatientUserById(Integer patient_user_id) {
+        return patientMapper.getPatientUserById(patient_user_id);
+    }
+
+    public PatientUser getPatientUserByAccount(String patient_account) {
+        return patientMapper.getPatientUserByAccount(patient_account);
+    }
+
+    public boolean updatePatientUser(PatientUser patientUser) {
+        try {
+            patientMapper.updatePatientUser(patientUser);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean deletePatientUser(Integer patient_user_id) {
+        try {
+            patientMapper.deletePatientUser(patient_user_id);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
